4e4cf584557a9d042e5d12d83ce9ce706060d919
khalique-hamza <software.eng.khalique@gmail.com>
2026-01-01 23:07:57 -0500

    added another test_case

--- test_function.py
+++ test_function.py
@@ -5,3 +5,6 @@ def test_direction_up():
 
 def test_direction_down():
     assert Direction.DOWN == "down", "Down direction not equal"
+
+def test_direction_left():
+    assert Direction.LEFT == "left", "Down direction not equal"
