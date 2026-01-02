7c248ae9fd0de520acad785f88dd555bf3147fc9
khalique <software.eng.khalique@gmail.com>
2026-01-01 21:50:43 -0500

    added some direction loop and a final print statement

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ def main():
     current_direction = Direction.UP
     print(f"The current direction is {current_direction}!")
 
+    direction_list = [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
+    for direction in direction_list:
+        print(f"Moving {direction}!")
+    
+    print("End of program!")
+
 main()
